eedf13dc72f98fd132e7215dbbd602948a540ec0
cc <votaryofturms@icloud.com>
2021-10-08 13:52:49 -0400

    fetched api data

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React from 'react'
+
+export default function Character(props) {
+
+    return (
+        <div>
+            <h2>Name: {props.name}</h2>
+            <p>Birth Year: {props.birth_year}</p>
+        </div>
+    )
+}
